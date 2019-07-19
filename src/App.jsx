97db96ad434fea927af233d6cea903c7af9e2861
Mathew Akre <mwakre29@gmail.com>
2019-07-19 13:37:41 -0700

    add structure to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import Error404 from './Error404';
 
 import './App.css';
 
 class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Switch>
+
+        </Switch>
+      </div>
+    )
+  }
 
 }
 
