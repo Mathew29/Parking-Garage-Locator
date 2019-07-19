62afc25df1a05666b7e85367f4f3459c2f2c87a4
Mathew Akre <mwakre29@gmail.com>
2019-07-19 15:26:22 -0700

    move to component folder

---
+++ src/components/GoogleMap.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+          lat: 45.5155,
+          lng: 122.6793
+        }} 
+      />
+    )
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'process.env.API_KEY'
+})(MapContainer);
