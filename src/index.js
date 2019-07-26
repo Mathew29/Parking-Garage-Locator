afa3390c1f36ebe2080b7cc5fc30432a4e031253
Mathew Akre <mwakre29@gmail.com>
2019-07-26 10:18:30 -0700

    add leaflet css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import 'leaflet/dist/leaflet.css';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
