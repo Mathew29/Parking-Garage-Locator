f51c7239c1250ff51005a6601d74bf3d03c01f41
Mathew Akre <mwakre29@gmail.com>
2019-07-26 10:18:48 -0700

    add route to leaflet map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Welcome from './Welcome';
 import Error404 from './Error404';
-import GoogleMap from './GoogleMap';
+import Leaflet from './Leaflet';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
@@ -13,7 +13,7 @@ class App extends React.Component {
       <div>
         <Switch>
           <Route exact path='/' component={Welcome} />
-          <Route path='/map' render={() => <GoogleMap />} />
+          <Route path='/map' render={() => <Leaflet />} />
           <Route component={Error404} />
         </Switch>
       </div>
